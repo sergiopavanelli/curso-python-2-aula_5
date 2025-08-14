ee9eab957997952e439a94de95e3dcb27b5b42f7
Sergio Pinton Pavanelli <sergio.pavanelli@ebserh.gov.br>
2025-08-14 15:26:22 -0300

    code update

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Emy', 2)
+bebida_suco = Bebida('Suco de Melancia', 5.0, 'grande')
+prato_paozinho = Prato('Paozinho', 2.0, 'O melhor pão da cidade')
+restaurante_praca.adicionar_bebida_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_prato_no_cardapio(prato_paozinho)
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_paozinho)
 
 if __name__ == '__main__':
     main()
